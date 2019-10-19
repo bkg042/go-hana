e59d755dd9ea77796995bd8c0a1a1bac2422f3e2
chimpy <bkghahremani@gmail.com>
2019-10-18 18:29:52 -1000

    finished the gamestate class

--- goboard_slow.py
+++ goboard_slow.py
@@ -84,7 +84,7 @@ class Board():
 		self.num_rows = num_rows
 		self._grid = {}
 
-	def place_stones(self, player, point):
+	def place_stone(self, player, point):
 		assert self.is_on_grid(point)
 		assert self._grid.get(point) is None
 		adjacent_same_color = []
@@ -143,3 +143,66 @@ class Board():
 					neighbor_string.add_liberty(point)
 			self._grid[point] = None
 
+class GameState():
+	"""
+	This describes the current state of the game.
+	"""
+
+	def __init__(self, board, next_player, previous, move):
+		self.board = board
+		self.next_player = next_player
+		self.previous_state = previous
+		self.last_move = move
+
+	def apply_move(self, move):
+		if move.is_play:
+			next_board = copy.deepcopy(self.board)
+			next_board.place_stone(self.next_player, move.point)
+		else:
+			next_board = self.board
+		return GameState(next_board, self.next_player.other, self, move)
+
+	def is_over(self):
+		if self.last_move is None:
+			return False
+		if self.last_move.is_resign:
+			return True
+		second_last_move = self.previous_state.last_move
+		if second_last_move is None:
+			return False
+		return self.last_move.is_pass and second_last_move.is_pass
+
+	def is_move_self_capture(self, player, move):
+		if not move.is_play:
+			return False
+		next_board = copy.deepcopy(self.board)
+		next_board.place_stone(player, move.point)
+		new_string = next_board.get_go_string(move.point)
+		return new_string.num_liberties == 0
+	
+	@property
+	def situation(self):
+		return (self.next_player, self.board)
+
+	def does_move_violate_ko(self, player, move):
+		if not move.is_play:
+			return False
+		next_board = copy.deepcopy(self.board)
+		next_board.place_stone(player, move.point)
+		next_situation = (player.other, next_board)
+		past_state = self.previous_state
+		while past_state is not None:
+			if past_state.situation == next_situation
+				return True
+			past_state = past_state.previous_state
+		return False
+
+	def is_valid_move(self, move):
+		if self.is_over:
+			return False
+		if move.is_pass or move.is_resign:
+			return True
+		return (
+			self.board.get(move.point) is None and
+			not self.is_move_self_capture(self.next_player, move) and
+			not self.does_move_violate_ko(self.next_player, move))	
